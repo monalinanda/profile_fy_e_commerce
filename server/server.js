3b2af9c9a9fb32aee99f3498bc209298049a2a32
monalinanda <monalinanda95@gmail.com>
2024-08-16 16:39:59 +0530

    fix in server file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
-const jsonServer = require('json-server');
+// See https://github.com/typicode/json-server#module
+import jsonServer from 'json-server';   
 const server = jsonServer.create();
 const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api', router);
 
-const port = process.env.PORT || 3001;
+// URL Rewriting Rules
+server.use(
+  jsonServer.rewriter({
+    '/api/products': '/products',
+    '/api/cart': '/cart',
+    '/blog/:resource/:id/show': '/:resource/:id'
+  })
+);
+
+// Use default router
+server.use(router);
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('JSON Server is running on port', port);
 });
+
+
+// Export the Server API
+export default server;
